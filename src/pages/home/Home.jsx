c47b0b9bea791bde70dbab4d5194b9ead0c4e034
Matteo Golin <matteo.golin@gmail.com>
2022-09-18 15:28:03 -0400

    Updated home page and styling.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,22 +6,16 @@ export default function Home() {
     <main id="home" className="page">
       <h1>Home</h1>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam est
-        eos vero nostrum beatae ipsa ea maxime? Facere odio omnis cupiditate
-        voluptatum dolore ullam consequuntur sint corporis, dicta repellendus
-        esse!
+        This a portfolio website. It is currently under construction and is not
+        optimized for mobile.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quisquam
-        enim dolorem possimus ab animi aspernatur, mollitia repellendus iste
-        nobis eveniet saepe harum quis impedit nulla cupiditate exercitationem
-        explicabo architecto.
+        There is nothing else to say at the moment because this is an unfinished
+        work.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum accusamus
-        quia, libero, repellendus similique quo dolores eaque quam culpa enim
-        consequatur quidem magnam voluptates modi! Doloribus itaque dolores
-        ipsam quis!
+        You should probably check out my socials and play around with the
+        themes, though.
       </p>
     </main>
   );
