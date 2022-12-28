777272c839b7a471e668f85ed9398d359c594551
Matteo Golin <matteo.golin@gmail.com>
2022-12-28 17:00:33 -0500

    Added auth token to API calls

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,7 +5,11 @@ export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(url)
+    fetch(url, {
+      headers: {
+        Authorization: `token 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`,
+      },
+    })
       .then((response) => response.json())
       .then((response_data) => {
         setData(response_data);
--- src/pages/cv/CV.jsx
+++ src/pages/cv/CV.jsx
@@ -6,8 +6,10 @@ import "./CV.css";
 export default function CV() {
   return (
     <main id="cv" className="page">
+      <h1>Resume</h1>
       <iframe
         id="resume"
+        title="resume"
         src="https://drive.google.com/file/d/1aBEuaLov5asqfqN0pCs10lqzlqjLLrFN/preview"
         allow="autoplay"
       />
